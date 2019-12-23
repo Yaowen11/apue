0e665bb0d0d4fdc7116a6dcbcfc74b61a4ec6ac7
Yaowen11 <760735372@qq.com>
2019-12-23 10:55:06 +0800

    update apue

--- ls.c
+++ ls.c
@@ -10,10 +10,11 @@ int main(int argc, char *argv[])
     DIR *dp;
     struct dirent *dirp;
     if (argc != 2)
-        printf("usage: ls dirctory_name");
-
+        err_quit("usage: ls dirctory_name");
     if ((dp = opendir(argv[1])) == NULL)
-        printf("can't open %s", argv[1]);
+        err_sys("can't open %s", argv[1]);
     while ((dirp = readdir(dp)) != NULL)
         printf("%s\n", dirp->d_name);
+    closedir(dp);
+    exit(0);
 }
